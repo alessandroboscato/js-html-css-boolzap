adae18e53c47cb5c4a3c12f0e63742ebebc2ce61
Alessandro <alessandro.boscato@gmail.com>
2020-09-08 17:49:54 +0200

    bozza messaggio jquery (errore)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
+
 $(document).ready(function() {
 
-  
+  $("#send-text").keydown(
+    function(event) {
+      if (event.which == 13) {
+        var inputValue = $("#send-text").val();
+
+        if (inputValue.lenght > 0) {
+          var elemento = $(".template .text-green p").clone();
+          $(".template p").append(elemento);
+          $("#send-text").val("";)
+        }
+      }
+    }
+  )
 });

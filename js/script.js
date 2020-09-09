04b06b7e4983ed5dd217733d4dac63f927af3a7f
Alessandro <alessandro.boscato@gmail.com>
2020-09-09 16:10:08 +0200

    Risposta bot

--- js/script.js
+++ js/script.js
@@ -5,18 +5,26 @@ $(document).ready(function() {
     function(event) {
       if (event.which == 13) {
         sendMessage();
+        $("#send-text").val("");
+        var answer = setTimeout(answerMessage, 1000);
       }
     }
-  )
+  );
 
   $(".send-message").click(
     function() {
       sendMessage();
+      var inputValue = $("#send-text").val();
+      if (inputValue != 0) {
+      var answer = setTimeout(answerMessage, 1000);
+      }
     }
-  )
+  );
+
+
 });
 
-
+// ------------------------------------------
 
 function sendMessage() {
   var inputValue = $("#send-text").val();
@@ -25,11 +33,21 @@ function sendMessage() {
     elemento.find(".message-text").addClass("sent");
     elemento.find("p").text(inputValue);
     $(".main-section-dx").append(elemento);
-    $("#send-text").val("");
     var date = new Date();
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var time = hours + ":" + minutes;
     elemento.find(".message-time").text(time);
   }
+}
+
+function answerMessage() {
+  var elemento = $(".template .message").clone();
+  elemento.find("p").text("ok");
+  $(".main-section-dx").append(elemento);
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var time = hours + ":" + minutes;
+  elemento.find(".message-time").text(time);
 }

e256652b96793a4602caad971e4dd6d812cff3b8
Alessandro <alessandro.boscato@gmail.com>
2020-09-09 15:50:07 +0200

    Funzione sendMessage() all'invio e al click

--- js/script.js
+++ js/script.js
@@ -4,32 +4,32 @@ $(document).ready(function() {
   $("#send-text").keydown(
     function(event) {
       if (event.which == 13) {
-        var inputValue = $("#send-text").val();
-        if (inputValue.length > 0) {
-          var elemento = $(".template .message").clone();
-          elemento.find(".message-text").addClass("sent");
-          elemento.find("p").text(inputValue);
-          $(".main-section-dx").append(elemento);
-          $("#send-text").val("");
-        }
+        sendMessage();
       }
+    }
+  )
+
+  $(".send-message").click(
+    function() {
+      sendMessage();
     }
   )
 });
 
-// function sendMessage() {
-//   var inputValue = $("#send-text").val();
-//   if (inputValue != "") {
-//     var templateMessage = $(".template .message").clone();
-//     console.log(templateMessage);
-//     var date = new Date();
-//     var hours = date.getHours();
-//     var minutes = date.getMinutes();
-//     var time = hours + ":" + minutes;
-//     templateMessage.find(".message-text").text(inputValue);
-//     templateMessage.find(".message-time").text("11:52");
-//     templateMessage.addClass("sent");
-//     $(".main-section-dx").append(templateMessage);
-//     $("#send-text").val("");
-//   }
-// }
+
+
+function sendMessage() {
+  var inputValue = $("#send-text").val();
+  if (inputValue != 0) {
+    var elemento = $(".template .message").clone();
+    elemento.find(".message-text").addClass("sent");
+    elemento.find("p").text(inputValue);
+    $(".main-section-dx").append(elemento);
+    $("#send-text").val("");
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var time = hours + ":" + minutes;
+    elemento.find(".message-time").text(time);
+  }
+}
